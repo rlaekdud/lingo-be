d1505b003d12b9164c2100ba066db655cb2db32a
rlaekdud <gloria030114@gmail.com>
2024-04-11 16:58:19 +0900

    initial commit
    - create new Spring project.

---
+++ src/main/java/kr/co/ipalab/lingobe/LingoBeApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ipalab.lingobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LingoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/ipalab/lingobe/LingoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.ipalab.lingobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
