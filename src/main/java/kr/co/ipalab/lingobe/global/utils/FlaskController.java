b54480b5df452923aee8a5db07b573c0b4bc7d2b
Kim Dayeong <gloria030114@gmail.com>
2024-05-07 15:44:28 +0900

    [feat: LINGO-7] add flask server controller
    - add flask server controller to manage request, response from flask server
    - add flask server url in application.yml

---
+++ src/main/java/kr/co/ipalab/lingobe/global/utils/FlaskController.java
@@ -0,0 +1,42 @@
+package kr.co.ipalab.lingobe.global.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class FlaskController {
+
+    @Value("${flask.url}")
+    private String flaskUrl;
+
+    public Object getFlaskResponse(String path, Object flaskRequestDto, Class<?> flaskResponseDtoClass) throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // Header set
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        // Body set
+        String requestBody = objectMapper.writeValueAsString(flaskRequestDto);
+
+        // HttpEntity set
+        HttpEntity<String> httpEntity = new HttpEntity<>(requestBody, httpHeaders);
+
+        // request to flask server
+        ResponseEntity<?> responseEntity = restTemplate.exchange(
+            flaskUrl + path,
+                HttpMethod.POST,
+                httpEntity,
+                flaskResponseDtoClass
+        );
+
+        // response object return
+        return responseEntity.getBody();
+    }
+}
