832ae953af954b9299fa2e95c074eb9c7ac72178
Kim Dayeong <gloria030114@gmail.com>
2024-05-15 17:26:38 +0900

    [feat: LINGO-9] add suggestion api dto(request, response)
    - add api > suggestion > dto package
    - add SuggestionRequestDto, SuggestionResponseDto classes

---
+++ src/main/java/kr/co/ipalab/lingobe/api/suggestion/dto/response/SuggestionResponseDto.java
@@ -0,0 +1,17 @@
+package kr.co.ipalab.lingobe.api.suggestion.dto.response;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SuggestionResponseDto {
+    private List<String> suggestions;
+}
