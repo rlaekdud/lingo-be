6774049dc1740fc182b994da37e993404c187bbb
Kim Dayeong <gloria030114@gmail.com>
2024-07-02 11:26:43 +0900

    fix: add cors mapping configuration

---
+++ src/main/java/kr/co/ipalab/lingobe/global/config/WebConfig.java
@@ -0,0 +1,20 @@
+package kr.co.ipalab.lingobe.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(false)
+            .maxAge(3600);
+    }
+
+}
